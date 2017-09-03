2a76154b2ca36dc4e6f1f25189a111fc9893f698
Ni_wuyu <a365893296@qq.com>
2017-09-03 23:27:54 +0800

    Niwuyu
    整理代码

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset=utf-8>
     <meta name="csrf_token" content="{{ csrf_token() }}">
+
     <title></title>
-    <link href=/static/css/app.f00a905f3ff72784b18f43e155a4733b.css rel=stylesheet>
+    <link href=/static/css/app.7c8701ee6dc917ff505dc87a769d3720.css rel=stylesheet>
 </head>
 <body>
 <div id=app></div>
-<script type=text/javascript src=/static/js/manifest.8ffeaa1631e124434668.js></script>
-<script type=text/javascript src=/static/js/vendor.a0701c69a7fb5899b383.js></script>
-<script type=text/javascript src=/static/js/app.bd976cc691cb96b48c11.js></script>
+<script type=text/javascript src=/static/js/manifest.d969239e8d93ae0097ac.js></script>
+<script type=text/javascript src=/static/js/vendor.1a1a486b0cb316de09ef.js></script>
+<script type=text/javascript src=/static/js/app.933e6b5e5990e01c3dad.js></script>
 </body>
 </html>
